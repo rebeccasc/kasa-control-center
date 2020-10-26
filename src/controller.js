6068531ccfb840fd6fa87b2b78d83fd75a069327
rebeccasc <65017227+rebeccasc@users.noreply.github.com>
2020-10-27 00:30:05 +0100

    Add option to dim bulbs

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,4 @@
 let { Client } = require('tplink-smarthome-api');
-const { default: BulbSchedule } = require('tplink-smarthome-api/lib/bulb/schedule');
-const { default: Dimmer } = require('tplink-smarthome-api/lib/plug/dimmer');
 
 let client = new Client();
 var backgroundOn = "#468153";
@@ -9,61 +7,72 @@ var backgroundOff = getComputedStyle(document.documentElement).getPropertyValue(
 // on load
 client.startDiscovery().on('device-new', (device) => {
   // device template
-  deviceTemplate = document.getElementById("device-template").cloneNode(true);
+  let deviceTemplate = document.getElementById("device-template").cloneNode(true);
   deviceTemplate.style.display = "block";
-  deviceButton = deviceTemplate.children[0].children[0];
+  let deviceButton = deviceTemplate.children[0].children[0];
   deviceButton.innerHTML = `${device.alias}`;
-  deviceSliderLabel = deviceTemplate.children[0].children[1];
-  deviceSlider = deviceTemplate.children[0].children[2];
-  deviceSliderPerc = deviceTemplate.children[0].children[3];
+  let deviceControl = deviceTemplate.children[1];
+  let deviceSlider = deviceControl.children[1];
+  let deviceLabel = deviceControl.children[2];
 
   // button listener
   deviceButton.addEventListener("click", function(){
     device.getPowerState().then( function(response){                               
         if(response == true){
           turnOff(device);
+          disableControlPanel(deviceControl);
         }
         else{
           turnOn(device);
+          enableControlPanel(deviceControl);
         }
     });
   });
 
   // set state-layout
   device.getPowerState().then( function(response){                               
-        if(response == true){
-          setDevicePanelBackground(device, backgroundOn)
-        }
-        else{
-          setDevicePanelBackground(device, backgroundOff)
-        }
+    if(response == true){
+      setDevicePanelBackground(device, backgroundOn)
+    }
+    else{
+      setDevicePanelBackground(device, backgroundOff)
+    }
   });
 
   // dimmable layout
-  if(device.deviceType == "bulb" && device.supportsBrightness){
-    // // set visible
-    // deviceSliderLabel.style.display = "block";
-    // deviceSlider.style.display = "block";
-    // deviceSliderPerc.style.display = "block";
-    
-    // console.log(device.supportsDimmer());
+  if(device.deviceType == "bulb" && device.supportsBrightness) {
+    // set visible
+    device.getPowerState().then(function(state){                               
+      if(state == true){
+        enableControlPanel(deviceControl);
+      }
+    });
 
-    // // add listener
-    // deviceSlider.oninput = function() {
-    //   console.log(this.value);      
-    // };
+    // set to last lightstate
+    device.lighting.getLightState().then(function(state){   
+      if(state.brightness != undefined){
+        deviceLabel.innerHTML = `${state.brightness}%`;
+        deviceSlider.value = state.brightness;
+      }
+    })    
+
+    //add listener
+    deviceSlider.oninput = function() {
+      setLightStateOfDevice(device, this.value);
+      deviceLabel.innerHTML = `${this.value}%`;
+    };
   }
 
   // add to view
-  mainContainer = document.getElementById("main-container");
+  let mainContainer = document.getElementById("main-container");
   mainContainer.appendChild(deviceTemplate)
 });
 
 function turnOnAll(){
-    client.startDiscovery().on('device-new', (device) => {
-      device.getSysInfo().then(console.log);
-      device.setPowerState(true);
-    });
+  client.startDiscovery().on('device-new', (device) => {
+    device.getSysInfo().then(console.log);
+    device.setPowerState(true);
+  });
 }
 
 function turnOn(device){
@@ -76,12 +85,31 @@ function turnOff(device){
   setDevicePanelBackground(device, backgroundOff)
 }
 
+function setLightStateOfDevice(device, dimValue){
+  device.lighting.setLightState({
+    on_off: 1,
+    brightness: Number(dimValue)
+  })
+}
+
 function setDevicePanelBackground(device, color){
-  devices = document.getElementById("main-container").children;
+  let devices = document.getElementById("main-container").children;
   for (let i = 1; i < devices.length; ++i) {
     if(devices[i].children[0].children[0].innerHTML == device.alias){
       devices[i].children[0].style.backgroundColor = `${color}`; 
       break;
     }
   }
+}
+
+function enableControlPanel(deviceControl){
+  deviceControl.children[1].disabled = false;
+  deviceControl.children[0].style.opacity = 1.0;
+  deviceControl.children[2].style.opacity = 1.0;
+}
+
+function disableControlPanel(deviceControl){
+  deviceControl.children[1].disabled = true;
+  deviceControl.children[0].style.opacity = 0.5;
+  deviceControl.children[2].style.opacity = 0.5;
 }
